8bdc3b5c8cf3eb3c7a8f6e09b724c6800dff5958
sbabiv <s.babiv@gmail.com>
2017-08-25 20:37:48 +0300

    Sequence increment

--- smpp/pdu/decoder.go
+++ smpp/pdu/decoder.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"net"
+	"fmt"
 )
 
 func HeaderDecoder(c net.Conn) (*Header, error) {
@@ -22,4 +23,33 @@ func HeaderDecoder(c net.Conn) (*Header, error) {
 		Status:   binary.BigEndian.Uint32(b[8:12]),
 		Sequence: binary.BigEndian.Uint32(b[12:16]),
 	}, nil
+}
+
+/*func Decode(c net.Conn) error {
+	h, err := HeaderDecoder(c)
+	if err != nil {
+		return err
+	}
+}*/
+
+func Decode(b []byte) {
+	h := &Header{
+		Length:   binary.BigEndian.Uint32(b[0:4]),
+		Id:       binary.BigEndian.Uint32(b[4:8]),
+		Status:   binary.BigEndian.Uint32(b[8:12]),
+		Sequence: binary.BigEndian.Uint32(b[12:16]),
+	}
+
+	n, systemId := decodeCoctet(b[16:], 16)
+	fmt.Printf("n: v%, systemid: v%", n, systemId)
+	fmt.Printf("len %v", h.Length)
+}
+
+func decodeCoctet(b []byte, offset int) (int, string) {
+	for i, v := range b {
+		if v == 0 {
+			return offset + i + 1, string(b[:i+1])
+		}
+	}
+	return 0, ""
 }
